34c0c3d7c66666ce5e2ae2e471f48d89759a1018
Ashishsawarkar98 <ashishsawarkar95@gmail.com>
2024-09-14 17:13:17 +0530

    Day 1

---
+++ src/day1/SecondProgramm.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class SecondProgramm {
+
+	public static void main(String[] args) {
+		System.out.println("Types of Testing :");
+		System.out.println("-----------------");
+		System.out.println("Manual Testing");
+		System.out.println("Automation Testing");
+		System.out.println("Database Testing");
+		System.out.println("API Testing");
+		System.out.println("----------------");
+
+
+	}
+
+}
