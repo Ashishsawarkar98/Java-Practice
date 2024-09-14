27a3e1d29fa6efd74e2606fc62409f4df46ba22c
Ashishsawarkar98 <ashishsawarkar95@gmail.com>
2024-09-14 17:35:31 +0530

    Day 1

---
+++ src/day1/ThirdProgramm.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class ThirdProgramm {
+
+	public static void main(String[]args) {
+		System.out.println("List Of Testing Tools:");
+		System.out.println("----------------------");
+		System.out.println("Browserstack");
+		System.out.println("Postman");
+		System.out.println("Selenium");
+		System.out.println("PGAdmin4");
+	}
+}
